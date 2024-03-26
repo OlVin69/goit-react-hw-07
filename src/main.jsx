e6e40adb30efdc433126f93233eb5dab3d7c1de7
OlVin69 <aviniichuk@gmail.com>
2024-03-26 09:47:25 +0200

    add modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "modern-normalize";
 import { Provider } from 'react-redux';
 import {store} from './redux/store.js';
 import  App  from './components/App/App.jsx';
